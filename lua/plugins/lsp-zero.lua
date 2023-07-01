587d1d31c2be1fb9de97252af135bfd2942aed00
Marces Engel <6208890+marcesengel@users.noreply.github.com>
2023-07-01 23:27:08 +0200

    feat: format typescriptreact using prettier

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -72,7 +72,7 @@ return {
         servers = {
           ['lua_ls'] = { 'lua' },
           ['rust_analyzer'] = { 'rust' },
-          ['null-ls'] = { 'typescript', 'javascript', 'json', 'html' }
+          ['null-ls'] = { 'typescript', 'typescriptreact', 'javascript', 'json', 'html' }
         }
       })
 
