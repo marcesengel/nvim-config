bb7cb3967ef712a2ed7b4ba5e9770129136d5386
Marces Engel <marces.engel@cashy.at>
2024-06-05 14:01:47 +0200

    feat: global search with <leader>g

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,6 +19,37 @@ return {
       vim.keymap.set('n', '<leader>ps', function()
         builtin.grep_string({ search = vim.fn.input("Grep > ") });
       end)
+
+      -- search for visual selection
+      function vim.getVisualSelection()
+        vim.cmd('noau normal! "vy"')
+        local text = vim.fn.getreg('v')
+        vim.fn.setreg('v', {})
+
+        text = string.gsub(text, "\n", "")
+        if #text > 0 then
+          return text
+        else
+          return ''
+        end
+      end
+
+      local keymap = vim.keymap.set
+      local opts = { noremap = true, silent = true }
+
+      --[[
+      keymap('n', '<space>G', ':Telescope current_buffer_fuzzy_find<cr>', opts)
+      keymap('v', '<space>G', function()
+        local text = vim.getVisualSelection()
+        builtin.current_buffer_fuzzy_find({ default_text = text })
+      end, opts)
+      ]]
+
+      keymap('n', '<space>g', ':Telescope live_grep<cr>', opts)
+      keymap('v', '<space>g', function()
+        local text = vim.getVisualSelection()
+        builtin.grep_string({ search = text })
+      end, opts)
     end,
     tag = '0.1.4',
     dependencies = {
